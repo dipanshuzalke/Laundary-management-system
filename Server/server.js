6c3f57e05c29e583550832ce6d579e7954b3b4ee
Dipanshu Zalke <dipanshuzalke@gmail.com>
2026-04-16 01:41:26 +0530

    fixed cors build error

--- Server/server.js
+++ Server/server.js
@@ -13,7 +13,14 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    'https://laundary-management-system-nu.vercel.app',
+    'http://localhost:5173'
+  ],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+  credentials: true
+}));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
